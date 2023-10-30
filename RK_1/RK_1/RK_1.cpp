ef030db6d46c5a38b08646645cb4c86a5e4c4617
L1L0wl <148867121+L1L0wl@users.noreply.github.com>
2023-10-30 20:21:00 +0300

    Add files via upload

---
+++ RK_1/RK_1/RK_1.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <string>
+#include <iomanip> // Добавляем для форматированного вывода
+using namespace std;
+
+const int MAX_PRODUCTS = 100; // Максимальное количество продуктов
+
+int main()
+{
+    setlocale(LC_ALL, "Russian"); // Устанавливаем русскую локаль
+
+    string products[MAX_PRODUCTS]; // Массив для названий товаров
+    int quantities[MAX_PRODUCTS];  // Массив для количества проданных единиц
+    double prices[MAX_PRODUCTS];   // Массив для цены за единицу
+
+    int numProducts;
+    cout << "Введите количество продуктов: ";
+    cin >> numProducts;
+
+    // Ввод данных о продажах
+    for (int i = 0; i < numProducts; i++) {
+        cout << "Введите название товара " << i + 1 << ": ";
+        cin >> products[i];
+
+        cout << "Введите количество проданных единиц: ";
+        cin >> quantities[i];
+
+        cout << "Введите цену за единицу: ";
+        cin >> prices[i];
+    }
+
+    // Вычисление общей суммы и средней цены
+    double totalSales = 0;
+    for (int i = 0; i < numProducts; i++) {
+        totalSales += quantities[i] * prices[i];
+    }
+    double averagePrice = totalSales / numProducts;
+
+    // Сортировка товаров по названиям
+    for (int i = 0; i < numProducts - 1; i++) {
+        for (int j = 0; j < numProducts - i - 1; j++) {
+            if (products[j] > products[j + 1]) {
+                swap(products[j], products[j + 1]);
+                swap(quantities[j], quantities[j + 1]);
+                swap(prices[j], prices[j + 1]);
+            }
+        }
+    }
+
+    // Вывод результатов
+    cout << "\nОбщая сумма продаж: " << fixed << setprecision(2) << totalSales << endl;
+    cout << "Средняя цена за единицу товара: " << fixed << setprecision(2) << averagePrice << endl;
+
+    cout << "\nОтсортированный список продуктов:\n";
+    for (int i = 0; i < numProducts; i++) {
+        cout << "Название: " << products[i] << ", Количество: " << quantities[i] << ", Цена: " << prices[i] << endl;
+    }
+
+    return 0;
+}
